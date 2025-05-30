e20ddafd63f3e9cfd47867b340a2a70ee5f9f8c8
JuanDavidAyala <jd.ayala15@ciaf.edu.co>
2025-05-30 15:32:19 -0500

    integracion de la grafica
    tuve que cambiar algunas cosas del css pues hacía que la grafica apareciera por debajo del total y los botones

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect
+import matplotlib.pyplot as plt
+import io
+import base64
 
 app = Flask(__name__)
 
@@ -8,11 +11,34 @@ carrito_de_compras = {}
 
 def index():
     total = sum(info['cantidad'] * info['precio'] for info in carrito_de_compras.values())
-    print("Total del carrito:", total)
-    return render_template("index.html", carrito = carrito_de_compras, total_pro=total)
+    mostrar_grafica = request.args.get('grafica') == '1'
+    imagen_grafica = generar_grafica() if mostrar_grafica else None
+    return render_template("index.html", carrito=carrito_de_compras, total_pro=total, imagen_grafica=imagen_grafica)
 
 
+def generar_grafica():
+    if not carrito_de_compras:
+        return None
+        #return "<p>No hay productos en el carrito para graficar.</p>"
 
+    productos = list(carrito_de_compras.keys())
+    cantidades = [info["cantidad"] for info in carrito_de_compras.values()]
+
+    fig, ax = plt.subplots()
+    ax.bar(productos, cantidades, color="lightcoral")
+    ax.set_title("Carrito de Compras")
+    ax.set_ylabel("Cantidad")
+    ax.set_xlabel("Producto")
+    plt.xticks(rotation=45, ha="right")
+
+    buffer = io.BytesIO()
+    plt.tight_layout()
+    fig.savefig(buffer, format="png")
+    plt.close(fig)
+    buffer.seek(0)
+
+    grafica_barras = base64.b64encode(buffer.read()).decode("utf-8")
+    return grafica_barras
 
 
 
@@ -24,19 +50,19 @@ def carrito():
         precio = request.form.get("precio")
         # Aquí puedes agregar la lógica para manejar el carrito de compras
         if producto in carrito_de_compras:
-            carrito_de_compras[producto]['cantidad'] += int(cantidad)
+            carrito_de_compras[producto]["cantidad"] += int(cantidad)
         else:
-            carrito_de_compras[producto] = {'cantidad': int(cantidad), 'precio': float(precio)}
-    return redirect('/')
+            carrito_de_compras[producto] = {"cantidad": int(cantidad), "precio": float(precio)}
+    return redirect("/")
 
 @app.route("/eliminar_producto", methods=["POST"])
 def eliminar_producto():
     producto = request.form.get("producto")
     if producto and producto in carrito_de_compras:
-        carrito_de_compras[producto]['cantidad'] -= 1
-        if carrito_de_compras[producto]['cantidad'] <= 0:
+        carrito_de_compras[producto]["cantidad"] -= 1
+        if carrito_de_compras[producto]["cantidad"] <= 0:
             carrito_de_compras.pop(producto)
-    return redirect('/')
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
